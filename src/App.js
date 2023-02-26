23f104f55bcc71ce4ba157f9712c997c926e7a88
rohit715131 <rohit715131@gmail.com>
2023-02-26 18:36:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./App.css";
 import MainRoute from "./pages/MainRoute";
 import Navbar from "./components/Navbar";
-import Footer from './components/Footer';
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
       <MainRoute />
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
